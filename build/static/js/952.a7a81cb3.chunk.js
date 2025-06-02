c6c95239153fdae1921eb5687de6a56b99b2faad
mcclellandkrm <karl@360spaces.co.uk>
2025-06-02 19:28:56 +0100

    Cleanup and prepare for Render deploy

--- build/static/js/952.a7a81cb3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkclient_intro_app=self.webpackChunkclient_intro_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.a7a81cb3.chunk.js.map
